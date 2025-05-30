8a7c08ca1b6da21fbad59b114c5eb97fbd29312f
palomaceg <p.canalaecheverria@gmail.com>
2025-05-30 19:45:41 +0200

    endpoints de todos los CRUD a falta de 2 que se harán en develop con los middleware. CRUD listos para testeo en Postman

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,6 +1,6 @@
-const { Product, Category } = require("../models/index.js");
+const { Product, Category, Sequelize } = require("../models/index.js");
 const product = require("../models/product.js");
-//const { Op } = Sequelize;
+const { Op } = Sequelize;
 
 const ProductController = {
   async create(req, res) {
@@ -56,6 +56,61 @@ const ProductController = {
             through: { attributes: [] },
           },
         ],
+      });
+      res.send(product);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  },
+  async getById(req, res) {
+    try {
+      const product = await Product.findByPk(req.params.id, {
+        attributes: ["id", "name", "price"],
+      });
+      res.send(product);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  },
+  async getAllByName(req, res) {
+    try {
+      const product = await Product.findAll({
+        where: { name: { [Op.like]: `%${req.params.name}%` } },
+        attributes: ["id", "name", "price"],
+      });
+      res.send(product);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  },
+  async getAllByPrice(req, res) {
+    try {
+      const product = await Product.findAll({
+        where: { price: { [Op.eq]: req.params.price } }, // Op.eq para encontrar un valor igual pasado por parámetro
+        attributes: ["id", "name", "price"],
+      });
+      res.send(product);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  },
+  async getAllByPriceRange(req, res) {
+    try {
+      const minprice = parseFloat(req.params.minprice);
+      const maxprice = parseFloat(req.params.maxprice);
+      const product = await Product.findAll({
+        where: { price: { [Op.between]: [minprice, maxprice] } }, // Op.between para encontrar por rango de precio
+        attributes: ["id", "name", "price"],
+      });
+      res.send(product);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  },
+  async getDescPrice(req, res) {
+    try {
+      const product = await Product.findAll({
+        order: [["price", "DESC"]],
       });
       res.send(product);
     } catch (error) {
